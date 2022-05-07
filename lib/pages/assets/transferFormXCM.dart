4ff9593a367159f0848f09270c3589d80d7b2d3e
shawn <dhryxy@gmail.com>
2022-05-07 13:31:28 +0800

    xcm transfer page estimate fee fix

--- lib/pages/assets/transferFormXCM.dart
+++ lib/pages/assets/transferFormXCM.dart
@@ -414,7 +414,9 @@ class _TransferFormXCMState extends State<TransferFormXCM> {
   }
 
   void _fetchData() {
-    _getTxFee();
+    if (_token != null) {
+      _getTxFee();
+    }
     _getAccountSysInfo();
   }
 
@@ -444,6 +446,11 @@ class _TransferFormXCMState extends State<TransferFormXCM> {
           _chainTo = tokenXcmConfig[0];
         }
       });
+
+      if (widget.plugin.sdk.api.connectedNode != null) {
+        // get tx fee while init state if connected
+        _getTxFee();
+      }
     });
   }
 

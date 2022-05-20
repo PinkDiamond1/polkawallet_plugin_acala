c54ceaff86d9756179a183eef8bb647889018279
shawn <dhryxy@gmail.com>
2022-05-20 12:13:50 +0800

    bump acala js sdk v4.1.2-21 & add new nft uri

--- lib/js_service_acala/src/constants/acala.ts
+++ lib/js_service_acala/src/constants/acala.ts
@@ -1,4 +1,5 @@
 export const nft_image_config = {
-  0: "https://acala.subdao.com/nft/metadata/AcalaCrowdloanContributor2021Metadata.json",
-  1: "https://acala.subdao.com/nft/metadata/AcalaCrowdloanQuestsMetadata.json",
+  0: "https://acala.polkawallet-cloud.com/nft/metadata/AcalaCrowdloanContributor2021Metadata.json",
+  1: "https://acala.polkawallet-cloud.com/nft/metadata/AcalaCrowdloanQuestsMetadata.json",
+  4: "https://acala.polkawallet-cloud.com/nft/metadata/AcalaEarlyAdopterMetadata.json",
 };
